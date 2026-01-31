4a4e8371289ee432a0535be21199dc40055a33a1
Ritesh Pandey <riteshpandey420123@gmail.com>
2026-02-01 03:28:46 +0530

    feat: complete signup POST API with request body mapping

--- server/src/main/java/com/educlips/server/controller/UserController.java
+++ server/src/main/java/com/educlips/server/controller/UserController.java
@@ -3,6 +3,9 @@ package com.educlips.server.controller;
 import com.educlips.server.dto.UserResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.educlips.server.dto.SignupRequest;
 
 @RestController
 public class UserController {
@@ -16,4 +19,15 @@ public class UserController {
                 "STUDENT"
         );
     }
+
+@PostMapping("/users/signup")
+public UserResponse signup(@RequestBody SignupRequest request) {
+
+    return new UserResponse(
+            1L,
+            request.getName(),
+            request.getEmail(),
+            request.getRole()
+    );
+}
 }
