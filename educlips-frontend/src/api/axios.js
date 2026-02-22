d9b4a25f5ebd713866760bc2d61cdcc037152a83
Ritesh Pandey <riteshpandey420123@gmail.com>
2026-02-22 22:40:41 +0530

    Frontend ready to deploy

--- educlips-frontend/src/api/axios.js
+++ educlips-frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 instance.interceptors.request.use((config) => {
